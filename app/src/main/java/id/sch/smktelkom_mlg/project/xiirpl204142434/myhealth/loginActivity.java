10c5481a6536e001d4a05e6fa9db924bf464b1e9
Ryan Farrel <ryanfarrel99@gmail.com>
2016-11-11 09:27:49 +0700

    Plum class loginActivity, gradle dependencies

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl204142434/myhealth/loginActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl204142434/myhealth/loginActivity.java
@@ -3,19 +3,13 @@ package id.sch.smktelkom_mlg.project.xiirpl204142434.myhealth;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.firebase.client.Firebase;
-
-
-public class signupActivity extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_login);
 
-        setTitle("Sign In");
-        Firebase.setAndroidContext(this);
-
+        setTitle("Login");
     }
-
 }
